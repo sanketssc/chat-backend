8d666f5c64e92c55f6681e7b774922283ae3c236
sanketssc <49080676+sanketssc@users.noreply.github.com>
2023-08-10 00:38:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { Server } = require("socket.io");
 const server = http.createServer();
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000/",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
