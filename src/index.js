bc44254a2aaf32b65bd0a756fe652c6a3671dd18
sanketssc <49080676+sanketssc@users.noreply.github.com>
2023-08-10 00:13:47 +0530

    added client url

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { Server } = require("socket.io");
 const server = http.createServer();
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://chat-app-rho-dusky.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
