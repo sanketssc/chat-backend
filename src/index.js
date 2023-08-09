476d79494ee5e1f63d83e226e58019d01bfa8650
sanketssc <49080676+sanketssc@users.noreply.github.com>
2023-08-10 00:23:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { Server } = require("socket.io");
 const server = http.createServer();
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-rho-dusky.vercel.app/",
+    origin: "http://localhost:3000/",
     methods: ["GET", "POST"],
   },
 });
